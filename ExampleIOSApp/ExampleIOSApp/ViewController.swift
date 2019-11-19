32a97de7d054a22a9671c12f158bc409c266ec1b
mrcormell <d.cormell@etoncollege.org.uk>
2019-11-19 09:13:31 +0000

    Initial check-in of iOS app

---
+++ ExampleIOSApp/ExampleIOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleIOSApp
+//
+//  Created by David Cormell on 19/11/2019.
+//  Copyright © 2019 David Cormell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
