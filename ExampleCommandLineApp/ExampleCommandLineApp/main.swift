3dbd2dcfaa31d367b6b88e1d121357fca3676ba9
mrcormell <d.cormell@etoncollege.org.uk>
2019-11-19 09:05:00 +0000

    Initial check-in of command line app

---
+++ ExampleCommandLineApp/ExampleCommandLineApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExampleCommandLineApp
+//
+//  Created by David Cormell on 19/11/2019.
+//  Copyright © 2019 David Cormell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
